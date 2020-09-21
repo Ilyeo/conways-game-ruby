6140346377933db55a5515b88b764ec244892a7d
Ilyeo <jrag.tkd@gmail.com>
2020-09-21 01:01:55 -0500

    Add god class
    Only God knows the destiny of the galaxies in the Universe

---
+++ test/god_test.rb
@@ -0,0 +1,53 @@
+require 'minitest/autorun'
+require_relative '../lib/god'
+require_relative '../lib/galaxy'
+
+describe God do
+  describe 'underpopulation' do
+    before do
+      @g = Galaxy.new(1) # live galaxy
+    end
+
+    it 'galaxy dies' do
+      @g.neighborhood_size = 1
+      God.destiny!(@g)
+      _(@g.alive?).must_equal false
+    end
+  end
+
+  describe 'survive' do
+    before do
+      @g = Galaxy.new(1) # live galaxy
+    end
+
+    it 'galaxy lives' do
+      @g.neighborhood_size = 3
+      God.destiny!(@g)
+      _(@g.alive?).must_equal true
+    end
+  end
+
+  describe 'overpopulation' do
+    before do
+      @g = Galaxy.new(1) # live galaxy
+    end
+
+    it 'galaxy dies' do
+      @g.neighborhood_size = 4
+      God.destiny!(@g)
+      _(@g.alive?).must_equal false
+    end
+  end
+
+  describe 'reproduction' do
+    before do
+      @g = Galaxy.new # dead galaxy
+    end
+
+    it 'galaxy dies' do
+      @g.neighborhood_size = 3
+      God.destiny!(@g)
+      _(@g.alive?).must_equal true
+    end
+  end
+end
